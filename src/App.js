a3db99a351595f434b54050d99aafe87dba3f285
Ivan Herrera Equihua <luis.herrera@mymoons.mx>
2022-06-20 13:28:48 -0500

    share coockies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useForm } from "react-hook-form";
+import Cookies from "js-cookie";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function saveUserSession(userData) {
+    Cookies.set("session", userData.user);
+    Cookies.set("session", userData.user, { path: "mymoons.mx" });
+    console.log(userData);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(saveUserSession)}>
+        <input type="text" {...register("user")}></input>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
